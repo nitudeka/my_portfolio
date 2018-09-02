9b917cf76b9c59102cadd45e67326ec50a872b34
nitudeka <nitumonideka327@gmail.com>
2018-09-02 12:55:50 +0530

    changed about page

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -6,24 +6,34 @@ const About = (props) => {
   return (
     <div className='about'>
       <div className='about__background'></div>
-      <div className='about__container'>
+      <div className='about__content'>
         <div className='avatar'>
           <img src={avatar} className='avatar__img' alt='avatar' />
         </div>
-        <div className='about__content'>
-          <span className='about__name'>Nitu Deka</span>
-          <h2 className='about__heading'>Hi, I am Nitu</h2>
-          <p className='about__paragraph'>I am a fullstack web developer who has a great ability of problem solving. I love everything about technology. I spend most of the time on a day in coding, trying to create beautiful content, like one you are viewing now. Wanna say Hi ? Just email me at nitumonideka327@gmail.com</p>
+        <h4 className='about__heading'>Nitu Deka</h4>
+        <div className='about__paragraphs'>
+          <p className='about__paragraph'>
+            Hey, you are here it means that you want to know about me. Let me introduce myself!
+          </p>
+          <p className='about__paragraph'>
+            I am from India. My complete name is <span className='bold-font'>Nitu Moni Deka</span>, a self-taught web developer with an addiction to mastering and adopting new skills. I want to work on complex tasks because I want to test and sharpen my skills.
+          </p>
+          <p className='about__paragraph'>
+            I want to satisfy my clients not just by doing what they say me to do, but by adding a little bit more from me to make the project even more beautiful.
+          </p>
+          <p className='about__paragraph'>
+            Like what you see? You can check me out on social media or can email me at <span className='bold-font'>nitumonideka327@gmail.com</span>
+          </p>
         </div>
         <div className='about__accounts'>
           <a href="https://www.facebook.com/profile.php?id=100008786917030&ref" rel="noopener noreferrer" target="_blank">
-            <Svg className='svg__about-facebook' name='facebook' />
+            <Svg className='svg__about-facebook accounts-link' name='facebook' />
           </a>
           <a href="https://github.com/nitudeka" rel="noopener noreferrer" target="_blank">
-            <Svg className='svg__about-github' name='github' />
+            <Svg className='svg__about-github accounts-link' name='github' />
           </a>
           <a href="https://www.linkedin.com/in/nitu-deka-12bb42160/" rel="noopener noreferrer" target="_blank">
-            <Svg className='svg__about-linkedin' name='linkedin' />
+            <Svg className='svg__about-linkedin accounts-link' name='linkedin' />
           </a>
         </div>
       </div>
@@ -32,3 +42,13 @@ const About = (props) => {
 }
 
 export default About;
+
+/*
+Hey, you are here it means that you want to know about me. Let me introduce myself!
+
+I am from India and my complete name is Nitu Moni Deka, a self-taught web developer with an addiction to mastering and adopting new skills. I truly love to work on complex tasks because I want to test my skills and sharpen them.
+
+I want to satisfy my clients not just by doing what they ask me to do, but by adding a little bit more from me to make the project more beautiful.
+
+Like what you see? You can check me out on social media or can email me at nitumonideka327@gmail.com
+*/
