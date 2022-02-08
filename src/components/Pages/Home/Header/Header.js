49cf327370391e733a5a38f1a7f527522f19ad20
iamnitudeka <iamnitudeka@gmail.com>
2022-02-08 10:00:56 +0530

    test

--- src/components/Pages/Home/Header/Header.js
+++ src/components/Pages/Home/Header/Header.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='header'>
-      <h1 className='heading__primary'>
-        <span className='heading__primary--main'>Nitu Deka</span>
-        <span className='heading__primary--sub'>Full stack web developer</span>
+    <div className="header">
+      <h1 className="heading__primary">
+        <span className="heading__primary--main">Nitu Deka</span>
+        <span className="heading__primary--sub">Full-stack web developer</span>
       </h1>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
